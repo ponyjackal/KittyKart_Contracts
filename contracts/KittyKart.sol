bd6d7692a360ededbe520723b060400023b6e62f
ponyjackal <pony1125@outlook.com>
2022-08-26 14:15:22 -0800

    feat: install openzeppelin and erc721a
    feat: install openzeppelin packages

--- contracts/KittyKart.sol
+++ contracts/KittyKart.sol
@@ -9,6 +9,17 @@
  |_|\_\_|\__|\__|\__, | |_____|_| |_|\__,_|
                   __/ |                    
                  |___/                                                    
+
+Website: https://kittyinuerc20.io/
+Telegram: https://t.me/kittyinutoken
+Twitter: https://twitter.com/KittyInuErc20
+Medium: https://medium.com/@kittyinu
+Github: https://github.com/KittyInu
+Instagram: https://www.instagram.com/kittyinuerc20/
+Facebook: https://www.facebook.com/profile.php?id=100073769243131
+
 */
 
 pragma solidity ^0.8.4;
+
+contract KittyKart {}
