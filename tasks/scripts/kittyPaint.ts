01734171a988cb91447e1a4cab51c2d0d38038da
benguyenkitty <benjamin@9vrse.org>
2022-08-31 11:37:31 -0700

    feat: add kittyPaint create table script

---
+++ tasks/scripts/kittyPaint.ts
@@ -0,0 +1,29 @@
+import { Signer } from "@ethersproject/abstract-signer";
+import { task } from "hardhat/config";
+import { TaskArguments } from "hardhat/types";
+
+import { KittyPaint } from "../../src/types/contracts/KittyPaint";
+import { KittyPaint__factory } from "../../src/types/factories/contracts/KittyPaint__factory";
+import { readContractAddress } from "../deploy/addresses/utils";
+import { readValue, writeValue } from "./values/utils";
+
+task("KittyPaint:createMetadataTable").setAction(async function (taskArguments: TaskArguments, { ethers }) {
+  const accounts: Signer[] = await ethers.getSigners();
+  const kittyPaintProxyAddress = readContractAddress("kittyPaintProxy");
+  const registryAddress = readValue("registry");
+
+  // attatch KittyPaint
+  const kittyPaintFactory: KittyPaint__factory = <KittyPaint__factory>(
+    await ethers.getContractFactory("KittyPaint", accounts[0])
+  );
+  const kittyPaint: KittyPaint = await kittyPaintFactory.attach(kittyPaintProxyAddress);
+
+  try {
+    const kittyPaintTableId = await kittyPaint.createMetadataTable(registryAddress);
+    console.log("kittyPaintTableId", kittyPaintTableId);
+    writeValue("kittyPaintTableId", kittyPaintTableId);
+    console.log("KittyPaint:createMetadataTable success");
+  } catch (err) {
+    console.log("KittyPaint:createMetadataTable error", err);
+  }
+});
