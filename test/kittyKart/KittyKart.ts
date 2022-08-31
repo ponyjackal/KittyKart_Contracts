a2f1c843fe3906879694ec94a5738113fe61b66e
benguyenkitty <benjamin@9vrse.org>
2022-08-31 14:43:35 -0800

    feat: add test script for kittyKart

--- test/kittyKart/KittyKart.ts
+++ test/kittyKart/KittyKart.ts
@@ -2,26 +2,29 @@ import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import { ethers } from "hardhat";
 
+import { DEPLOY_ADDRESS } from "../constants";
 import type { Signers } from "../types";
-import { shouldBehaveLikeGreeter } from "./Greeter.behavior";
-import { deployGreeterFixture } from "./Greeter.fixture";
+import { shouldBehaveLikeKittyKart } from "./KittyKart.behavior";
+import { deployKittyKartFixture } from "./KittyKart.fixture";
 
 describe("Unit tests", function () {
   before(async function () {
     this.signers = {} as Signers;
 
     const signers: SignerWithAddress[] = await ethers.getSigners();
+    // impersonate account on goerli
+    this.signers.deployer = await ethers.getImpersonatedSigner(DEPLOY_ADDRESS);
     this.signers.admin = signers[0];
 
     this.loadFixture = loadFixture;
   });
 
-  describe("Greeter", function () {
+  describe("KittyKart", function () {
     beforeEach(async function () {
-      const { greeter } = await this.loadFixture(deployGreeterFixture);
-      this.greeter = greeter;
+      const { kittyKart } = await this.loadFixture(deployKittyKartFixture);
+      this.kittyKart = kittyKart;
     });
 
-    shouldBehaveLikeGreeter();
+    shouldBehaveLikeKittyKart();
   });
 });
