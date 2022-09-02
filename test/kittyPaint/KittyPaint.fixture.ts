841ac18271368382150044d4a8a2d11489f14afc
benguyenkitty <benjamin@9vrse.org>
2022-09-02 06:20:21 -0700

    feat: create table and mint tokens on fixture

--- test/kittyPaint/KittyPaint.fixture.ts
+++ test/kittyPaint/KittyPaint.fixture.ts
@@ -3,15 +3,19 @@ import { ethers, upgrades } from "hardhat";
 
 import type { KittyPaint } from "../../src/types/contracts/KittyPaint";
 import { KittyPaint__factory } from "../../src/types/factories/contracts/KittyPaint__factory";
-import { DEPLOY_ADDRESS } from "../constants";
+import { DEPLOY_ADDRESS, REGISTRY_ADDRESS } from "../constants";
 
 export async function deployKittyPaintFixture(): Promise<{ kittyPaint: KittyPaint }> {
   const signers: SignerWithAddress[] = await ethers.getSigners();
   const admin: SignerWithAddress = signers[0];
   const deployer: SignerWithAddress = await ethers.getImpersonatedSigner(DEPLOY_ADDRESS);
-
+  // deploy KittyPaint
   const kittyPaintFactory: KittyPaint__factory = await ethers.getContractFactory("KittyPaint", deployer);
   const kittyPaint: KittyPaint = <KittyPaint>await upgrades.deployProxy(kittyPaintFactory, ["", "", admin.address]);
   await kittyPaint.deployed();
+  // create table
+  await kittyPaint.createMetadataTable(REGISTRY_ADDRESS);
+  // mint 10 tokens to admin
+  await kittyPaint.connect(admin).publicMint(10);
   return { kittyPaint };
 }
