4e2fb7cc8d014887036beda6c9fd43b736247ec8
benguyenkitty <benjamin@9vrse.org>
2022-09-02 06:17:16 -0700

    feat: update fixture script for test

---
+++ test/autoBodyShop/AutoBodyShop.behavior.ts
@@ -0,0 +1,7 @@
+import { expect } from "chai";
+
+import { REGISTRY_ADDRESS } from "../constants";
+
+export function shouldBehaveLikeAutoBodyShop(): void {
+  it("should create metadata table", async function () {});
+}
--- test/types.ts
+++ test/types.ts
@@ -1,5 +1,6 @@
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 
+import type { AutoBodyShop } from "../src/types/contracts/AutoBodyShop";
 import type { KittyKart } from "../src/types/contracts/KittyKart";
 import type { KittyPaint } from "../src/types/contracts/KittyPaint";
 
@@ -7,6 +8,7 @@ type Fixture<T> = () => Promise<T>;
 
 declare module "mocha" {
   export interface Context {
+    autoBodyShop: AutoBodyShop;
     kittyKart: KittyKart;
     kittyPaint: KittyPaint;
     loadFixture: <T>(fixture: Fixture<T>) => Promise<T>;
