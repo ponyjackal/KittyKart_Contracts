66ccdea7fca00ab9bf15830b630f6cca3c491816
ponyjackal <pony1125@outlook.com>
2022-09-02 06:20:21 -0700

    feat: create table and mint tokens on fixture

--- test/kittyKart/KittyKart.fixture.ts
+++ test/kittyKart/KittyKart.fixture.ts
@@ -3,15 +3,19 @@ import { ethers, upgrades } from "hardhat";
 
 import type { KittyKart } from "../../src/types/contracts/KittyKart";
 import { KittyKart__factory } from "../../src/types/factories/contracts/KittyKart__factory";
-import { DEPLOY_ADDRESS } from "../constants";
+import { DEPLOY_ADDRESS, REGISTRY_ADDRESS } from "../constants";
 
 export async function deployKittyKartFixture(): Promise<{ kittyKart: KittyKart }> {
   const signers: SignerWithAddress[] = await ethers.getSigners();
   const admin: SignerWithAddress = signers[0];
   const deployer: SignerWithAddress = await ethers.getImpersonatedSigner(DEPLOY_ADDRESS);
-
+  // deploy KittyKart
   const kittyKartFactory: KittyKart__factory = await ethers.getContractFactory("KittyKart", deployer);
   const kittyKart: KittyKart = <KittyKart>await upgrades.deployProxy(kittyKartFactory, ["", "", admin.address]);
   await kittyKart.deployed();
+  // create table
+  await kittyKart.createMetadataTable(REGISTRY_ADDRESS);
+  // mint 10 tokens to admin
+  await kittyKart.connect(admin).publicMint(10);
   return { kittyKart };
 }
