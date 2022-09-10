34daf97cf352f892d4fb2e737fb0a5861e52cd9d
benguyenkitty <benjamin@9vrse.org>
2022-09-10 09:30:08 -0700

    feat: update test case

--- test/kittyKartAsset/KittyKartAsset.fixture.ts
+++ test/kittyKartAsset/KittyKartAsset.fixture.ts
@@ -13,6 +13,9 @@ import {
   DEPLOY_ADDRESS,
   GAME_SERVER_ADDRESS,
   REGISTRY_ADDRESS,
+  SIGNATURE_TYPES,
+  SIGNATURE_VERSION,
+  SIGNING_DOMAIN,
 } from "../constants";
 
 export async function deploykittyKartAssetFixture(): Promise<{ kittyKartAsset: KittyKartAsset }> {
@@ -37,52 +40,37 @@ export async function deploykittyKartAssetFixture(): Promise<{ kittyKartAsset: K
   // create table
   await kittyKartAsset.connect(deployer).createMetadataTable(REGISTRY_ADDRESS);
   // set game server
-  await kittyKartAsset.connect(deployer).setGameServer(gameServer.address);
-  // mint 4 tokens to alice
-  await kittyKartAsset
-    .connect(gameServer)
-    .safeMint(
-      alice.address,
-      [
-        ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("string"), 0, 16),
-        ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("string"), 0, 16),
-        ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("string"), 0, 16),
-      ],
-      [
-        ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("paint"), 0, 16),
-        ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("wheel"), 0, 16),
-        ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("engine"), 0, 16),
-      ],
-      [
-        ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("blue"), 0, 16),
-        ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("Alloy"), 0, 16),
-        ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("v8"), 0, 16),
-      ],
-    );
-  await kittyKartAsset
-    .connect(gameServer)
-    .safeMint(
-      alice.address,
-      [ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("string"), 0, 16)],
-      [ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("paint"), 0, 16)],
-      [ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("pink"), 0, 16)],
-    );
-  await kittyKartAsset
-    .connect(gameServer)
-    .safeMint(
-      alice.address,
-      [
-        ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("string"), 0, 16),
-        ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("string"), 0, 16),
-      ],
-      [
-        ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("wheel"), 0, 16),
-        ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("engine"), 0, 16),
-      ],
-      [
-        ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("Alloy"), 0, 16),
-        ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("v8"), 0, 16),
-      ],
-    );
+  await kittyKartAsset.connect(deployer).setGameServer(admin.address);
+  // sign a message for KittyAssetVoucher
+  let voucher = {
+    receiver: alice.address,
+    displayTypes: [
+      ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("paint"), 0, 16),
+      ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("wheel"), 0, 16),
+      ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("engine"), 0, 16),
+    ],
+    traitTypes: [
+      ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("paint"), 0, 16),
+      ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("wheel"), 0, 16),
+      ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("engine"), 0, 16),
+    ],
+    values: [
+      ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("blue"), 0, 16),
+      ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("Alloy"), 0, 16),
+      ethers.utils.hexDataSlice(ethers.utils.formatBytes32String("v8"), 0, 16),
+    ],
+    signature: "",
+  };
+  const typedDomain = {
+    name: SIGNING_DOMAIN,
+    version: SIGNATURE_VERSION,
+    chainId: 1,
+    verifyingContract: kittyKartAsset.address,
+  };
+  const signature = await admin._signTypedData(typedDomain, SIGNATURE_TYPES, voucher);
+  voucher.signature = signature;
+
+  // mint 3 tokens to alice
+  await kittyKartAsset.connect(alice).safeMint(voucher);
   return { kittyKartAsset };
 }
