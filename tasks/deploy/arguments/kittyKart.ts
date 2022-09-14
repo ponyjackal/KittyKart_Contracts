de674e9e13031de4fcec230dbeb28e711b5a5e3b
benguyenkitty <benjamin@9vrse.org>
2022-09-14 13:21:16 -0700

    feat: update deploy script and test cases for KittyAsset

--- tasks/deploy/arguments/kittyKart.ts
+++ tasks/deploy/arguments/kittyKart.ts
@@ -1,8 +1,8 @@
 import { readValue } from "../../scripts/values/utils";
 
 const BASE_URI = readValue("baseURI");
-const DESCRIPTION = readValue("assetDescription");
-const IMAGE = readValue("assetImage");
+const DESCRIPTION = readValue("kartDescription");
+const IMAGE = readValue("kartImage");
 const EXTERNAL_URL = readValue("kartExternalURL");
 const ANIMATION_URL = readValue("kartAnimationURL");
 const ROYALTY_RECEIVER = "0xE078c3BDEe620829135e1ab526bE860498B06339";
