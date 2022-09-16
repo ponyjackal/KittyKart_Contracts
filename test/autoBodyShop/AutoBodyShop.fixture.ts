08daa1a0a6f587395f81e2a8c1597f09013b8c47
ponyjackal <pony1125@outlook.com>
2022-09-16 11:55:10 -0700

    feat: add main task to initialize contracts

--- test/autoBodyShop/AutoBodyShop.fixture.ts
+++ test/autoBodyShop/AutoBodyShop.fixture.ts
@@ -5,6 +5,10 @@ import type { AutoBodyShop } from "../../src/types/contracts/AutoBodyShop";
 import { AutoBodyShop__factory } from "../../src/types/factories/contracts/AutoBodyShop__factory";
 import { DEPLOY_ADDRESS, REGISTRY_ADDRESS } from "../constants";
 
+/**
+ * @note AutoBodyShop contract will be deprecated once we build out backend service for it
+ */
+
 export async function deployAutoBodyShopFixture(): Promise<{ autoBodyShop: AutoBodyShop }> {
   const signers: SignerWithAddress[] = await ethers.getSigners();
   const deployer: SignerWithAddress = await ethers.getImpersonatedSigner(DEPLOY_ADDRESS);
