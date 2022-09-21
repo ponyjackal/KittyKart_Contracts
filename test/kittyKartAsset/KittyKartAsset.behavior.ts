81b830aef040ceabb11fde944f433e5ff397af75
benguyenkitty <benjamin@9vrse.org>
2022-09-21 15:17:22 -0800

    feat: add test cases for grantAccess

--- test/kittyKartAsset/KittyKartAsset.behavior.ts
+++ test/kittyKartAsset/KittyKartAsset.behavior.ts
@@ -189,6 +189,20 @@ export function shouldBehaveLikekittyKartAsset(): void {
     });
   });
 
+  describe("grantAccess", async function () {
+    it("should be reverted for not owner", async function () {
+      const tx = this.kittyKartAsset
+        .connect(this.signers.alice)
+        .grantAccess("0xFB6c5feE537344Db0f585d65C684fbc2A800d0a8", true, true, true);
+      await expect(tx).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+
+    it("should update externalUrl and emit an event on setExternalURL", async function () {
+      const tx = this.kittyKartAsset.grantAccess("0xFB6c5feE537344Db0f585d65C684fbc2A800d0a8", true, true, true);
+      await expect(tx).to.be.emit(this.kittyKartAsset, "AcessGranted");
+    });
+  });
+
   describe("MarketplaceRestriction", async function () {
     it("should not allow approve if marketplace is not set", async function () {
       // approve
