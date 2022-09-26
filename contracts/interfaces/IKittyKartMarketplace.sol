22d0381f985963c5951e365f2c5c53ab8afcb471
ponyjackal <pony1125@outlook.com>
2022-09-26 06:53:18 -0700

    feat: add makeOffer function

--- contracts/interfaces/IKittyKartMarketplace.sol
+++ contracts/interfaces/IKittyKartMarketplace.sol
@@ -2,6 +2,28 @@
 pragma solidity ^0.8.12;
 
 interface IKittyKartMarketplace {
+  // -----------------------------------------
+  // KittyKartGoKart Events
+  // -----------------------------------------
+  event NFTListed(address indexed tokenContract, uint256 indexed tokenId, address indexed seller, uint256 price);
+  event NFTSold(address indexed tokenContract, uint256 indexed tokenId, address seller, address buyer, uint256 price);
+  event OfferMade(address indexed tokenContract, uint256 indexed tokenId, address indexed buyer, uint256 price);
+
+  struct NFT {
+    address nftContract;
+    uint256 tokenId;
+    address seller;
+    address owner;
+    bool listed;
+    uint256 price;
+  }
+
+  struct Offer {
+    bool exists;
+    address buyer;
+    uint256 price;
+  }
+
   function list(
     address _tokenContract,
     uint256 _tokenId,
@@ -16,15 +38,15 @@ interface IKittyKartMarketplace {
     uint256 _amount
   ) external;
 
-  function offer(
+  function makeOffer(
     address _tokenContract,
     uint256 _tokenId,
     uint256 _amount
   ) external;
 
-  function sellNFT(
-    address _tokenContract,
-    uint256 _tokenId,
-    uint256 _amount
-  ) external;
+  // function sellNFT(
+  //   address _tokenContract,
+  //   uint256 _tokenId,
+  //   uint256 _amount
+  // ) external;
 }
