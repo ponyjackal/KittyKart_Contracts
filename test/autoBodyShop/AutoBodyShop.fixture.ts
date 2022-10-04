b13bf902f07f19074e5617824f4de8d730038005
benguyenkitty <benjamin@9vrse.org>
2022-10-03 21:41:08 -0700

    feat: update unit test for autobodyshop

--- test/autoBodyShop/AutoBodyShop.fixture.ts
+++ test/autoBodyShop/AutoBodyShop.fixture.ts
@@ -3,7 +3,12 @@ import { ethers, upgrades } from "hardhat";
 
 import type { AutoBodyShop } from "../../src/types/contracts/AutoBodyShop";
 import { AutoBodyShop__factory } from "../../src/types/factories/contracts/AutoBodyShop__factory";
-import { DEPLOY_ADDRESS, REGISTRY_ADDRESS } from "../constants";
+import {
+  DEPLOY_ADDRESS,
+  KITTY_KART_ASSET_ATTRIBUTE_TABLE_ID,
+  KITTY_KART_GO_KART_TABLE_ID,
+  REGISTRY_ADDRESS,
+} from "../constants";
 
 /**
  * @note AutoBodyShop contract will be deprecated once we build out backend service for it
@@ -21,6 +26,9 @@ export async function deployAutoBodyShopFixture(): Promise<{ autoBodyShop: AutoB
   await autoBodyShop.deployed();
   // set game server
   await autoBodyShop.connect(deployer).setGameServer(gameSever.address);
-
+  // set kart table
+  await autoBodyShop.connect(deployer).setKittyKartGoKartTableId(KITTY_KART_GO_KART_TABLE_ID);
+  // set attribute table
+  await autoBodyShop.connect(deployer).setKittyKartAssetAttributeTableId(KITTY_KART_ASSET_ATTRIBUTE_TABLE_ID);
   return { autoBodyShop };
 }
