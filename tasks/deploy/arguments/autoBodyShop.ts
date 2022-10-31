796d56891a08e78391e2cf35f56f7c510e2a3474
ponyjackal <pony1125@outlook.com>
2022-10-31 06:54:19 -0700

    feat: add deploy script for marketplace

--- tasks/deploy/arguments/autoBodyShop.ts
+++ tasks/deploy/arguments/autoBodyShop.ts
@@ -1,13 +1,13 @@
 import { readValue } from "../../scripts/values/utils";
 import { readContractAddress } from "../addresses/utils";
 
-const KITTY_KART_ADDRESS = readContractAddress("kittyKartGoKartProxy");
-const KITTY_PAINT_ADDRESS = readContractAddress("kittyKartAssetProxy");
+const KITTY_KART_GO_KART_ADDRESS = readContractAddress("kittyKartGoKartProxy");
+const KITTY_KART_ASSET_ADDRESS = readContractAddress("kittyKartAssetProxy");
 const REGISTRY_ADDRESS = readValue("registry");
 
 const values = {
-  KITTY_KART_ADDRESS,
-  KITTY_PAINT_ADDRESS,
+  KITTY_KART_GO_KART_ADDRESS,
+  KITTY_KART_ASSET_ADDRESS,
   REGISTRY_ADDRESS,
 } as const;
 
