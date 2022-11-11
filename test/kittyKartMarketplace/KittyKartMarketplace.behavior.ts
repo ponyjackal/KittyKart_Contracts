d2368d14fa6ca4b8b8ce3b64d89d414e0adc373a
benguyenkitty <benjamin@9vrse.org>
2022-11-11 06:41:11 -0800

    feat: add unit test for buy

--- test/kittyKartMarketplace/KittyKartMarketplace.behavior.ts
+++ test/kittyKartMarketplace/KittyKartMarketplace.behavior.ts
@@ -314,16 +314,15 @@ export function shouldBehaveLikeKittyKartMarketplace(): void {
 
   describe("Buy", async function () {
     before(async function () {
-      // alice listed kart_2 and asset_2
-      // alice is going to list kart
+      // alice is going to list a kart_2
       // sign a message for KittyKartMarketplace
       const data = {
         user: this.signers.alice.address,
         collection: this.kittyKartGoKart.address,
         tokenId: 2,
-        price: ethers.utils.parseEther("0.1"),
+        price: ethers.utils.parseEther("0.5"),
         actionType: 0,
-        nonce: 0,
+        nonce: 2,
         expiry: 0,
       };
       const typedDomain = {
@@ -340,41 +339,40 @@ export function shouldBehaveLikeKittyKartMarketplace(): void {
       // list nft
       await this.kittyKartMarketplace.connect(this.signers.alice).list(voucher);
 
-      // alice is going to list kart
+      // alice is going to list an asset_1
       // sign a message for KittyKartMarketplace
       const data1 = {
         user: this.signers.alice.address,
         collection: this.kittyKartAsset.address,
-        tokenId: 2,
+        tokenId: 1,
         price: ethers.utils.parseEther("0.1"),
         actionType: 0,
-        nonce: 1,
+        nonce: 3,
         expiry: 0,
       };
-      const typedDomain1 = {
-        name: SIGNING_MARKETPLACE_DOMAIN,
-        version: SIGNING_MARKETPLACE_VERSION,
-        chainId: network.config.chainId,
-        verifyingContract: this.kittyKartMarketplace.address,
-      };
-      const signature1 = await this.signers.gameServer._signTypedData(typedDomain1, SIGNING_MARKETPLACE_TYPES, data);
+      const signature1 = await this.signers.gameServer._signTypedData(typedDomain, SIGNING_MARKETPLACE_TYPES, data1);
       const voucher1 = {
         ...data1,
         signature: signature1,
       };
       // list nft
       await this.kittyKartMarketplace.connect(this.signers.alice).list(voucher1);
+
+      // bell allows KittyInu tokens to KittyKartMarketplace
+      await this.kittyInu
+        .connect(this.signers.bell)
+        .increaseAllowance(this.kittyKartMarketplace.address, ethers.utils.parseEther("100"));
     });
 
     it("should buy a listed nft", async function () {
       // bell is going to buy a kart_2
       // sign a message for KittyKartMarketplace
       const data = {
-        user: this.signers.alice.address,
+        user: this.signers.bell.address,
         collection: this.kittyKartGoKart.address,
         tokenId: 2,
-        price: 0, // here price will be ignored
-        actionType: 0,
+        price: ethers.utils.parseEther("0.5"),
+        actionType: 1,
         nonce: 0,
         expiry: 0,
       };
@@ -392,17 +390,17 @@ export function shouldBehaveLikeKittyKartMarketplace(): void {
       // list nft
       await this.kittyKartMarketplace.connect(this.signers.bell).buyNFT(voucher);
       // check ownership is transferred
-      const ownerOfKart = await this.kittyKartGoKart.ownerOf(0);
-      expect(ownerOfKart).to.equal(this.kittyKartMarketplace.address);
-      // check kart is listed
-      const listedKart = await this.kittyKartMarketplace._idToNFT(this.kittyKartGoKart.address, 0);
-      expect(listedKart.nftContract).to.equal(this.kittyKartGoKart.address);
-      expect(listedKart.tokenId).to.equal(0);
-      expect(listedKart.seller).to.equal(this.signers.alice.address);
-      expect(listedKart.listed).to.equal(true);
-      expect(listedKart.price).to.equal(ethers.utils.parseEther("0.1"));
+      const ownerOfKart = await this.kittyKartGoKart.ownerOf(2);
+      expect(ownerOfKart).to.equal(this.signers.bell.address);
+      // check if alice got KittyInu token reducing the market fee 5%
+      const balanceOfAlice = await this.kittyInu.balanceOf(this.signers.alice.address);
+      expect(balanceOfAlice).to.equal(ethers.utils.parseEther("0.475"));
+      // check kart is removed from the list
+      const listedKart = await this.kittyKartMarketplace._idToNFT(this.kittyKartGoKart.address, 2);
+      expect(listedKart.listed).to.equal(false);
+      expect(listedKart.price).to.equal(0);
       // check if nonce is updated
-      const nonce = await this.kittyKartMarketplace.nonces(this.signers.alice.address);
+      const nonce = await this.kittyKartMarketplace.nonces(this.signers.bell.address);
       expect(nonce).to.equal(1);
       // check if signature is set as used
       const isSignatureUsed = await this.kittyKartMarketplace.signatures(signature);
